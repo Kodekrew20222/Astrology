4e737295e545de60ba9dbc1a3aa65ead56f3e099
RohanLlali <rohan.lali2002@gmail.com>
2026-03-31 13:58:01 +0530

    changes

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -25,7 +25,7 @@ export async function handler(event) {
     const timeout = setTimeout(() => controller.abort(), 8000); // 8 sec
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: {
